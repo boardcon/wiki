d5630b36fb69bf4910a24dcd8950adcdeda3c0a2
boardcon <105197449+boardcon@users.noreply.github.com>
2022-05-25 09:51:11 +0800

    Update conf.py

--- docs/source/hardware/conf.py
+++ docs/source/hardware/conf.py
@@ -2,10 +2,9 @@
 
 # -- Project information
 
-project = 'Computer_Architecture_Notebook'
-copyright = '2022, kkqq'
-author = 'kkqq'
-
+project = 'EM3288 Hardware test'
+copyright = '2022, Boardcon'
+author = 'boardcon'
 release = '1.0'
 version = '1.0.0'
 
